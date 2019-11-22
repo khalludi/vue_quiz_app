40b80779be149323d4f3dd50698dec933fe3df43
Khalid Ali <khalludi123@gmail.com>
2019-11-22 14:39:14 -0500

    Display answer choices

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -7,12 +7,19 @@
 
         <hr class="my-4">
 
-        <p>
-          List of answers
-        </p>
+        <b-list-group>
+          <b-list-group-item
+            v-for="(answer,index) in answers"
+            :key="index"
+            @click="selectAnswer(index)"
+            :class="[selectedIndex === index ? 'selected' : '']"
+          >
+            {{ answer }}
+          </b-list-group-item>
+        </b-list-group>
 
         <b-button variant="primary" href="#">Submit</b-button>
-        <b-button variant="success" href="#">Next</b-button>
+        <b-button @click="next" variant="success" href="#">Next</b-button>
       </b-jumbotron>
     </div>
 </template>
@@ -20,7 +27,61 @@
 <script>
 export default {
   props: {
-    currentQuestion: Object
+    currentQuestion: Object,
+    next: Function
+  },
+  data() {
+    return {
+      selectedIndex: null
+    }
+  },
+  computed: {
+    answers() {
+      let answers = [...this.currentQuestion.incorrect_answers]
+      answers.push(this.currentQuestion.correct_answer)
+      return answers
+    }
+  },
+  watch: {
+    currentQuestion() {
+      this.selectedIndex = null
+      this.shuffleAnswers()
+    }
+  },
+  methods: {
+    selectAnswer(index) {
+      this.selectedIndex = index
+    },
+    shuffleAnswers() {
+      let answers = [...this.currentQuestion.incorrect_answers, this.currentQuestion.correct_answer]
+    }
   }
 }
 </script>
+
+<style scoped>
+.list-group {
+  margin-bottom: 15px;
+}
+
+.list-group-item:hover {
+  background: #eee;
+  cursor: pointer;
+}
+
+.btn {
+  margin: 0 5px;
+}
+
+.selected {
+  background-color: rgb(0, 174, 255);
+}
+
+.correct {
+  background-color: lightgreen;
+}
+
+.incorrect {
+  background-color: lightcoral
+}
+</style>
