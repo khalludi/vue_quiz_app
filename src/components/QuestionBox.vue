8ccbb4e0c2d482b6872b2a8784c732aab8baa1f1
Khalid Ali <khalludi123@gmail.com>
2019-11-22 14:18:05 -0500

    Get and show question from quiz api

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -1,3 +1,26 @@
 <template>
-    <h1>Question Box</h1>
+    <div class="question-box-container">
+      <b-jumbotron>
+        <template v-slot:lead>
+          {{ currentQuestion.question }}
+        </template>
+
+        <hr class="my-4">
+
+        <p>
+          List of answers
+        </p>
+
+        <b-button variant="primary" href="#">Submit</b-button>
+        <b-button variant="success" href="#">Next</b-button>
+      </b-jumbotron>
+    </div>
 </template>
+
+<script>
+export default {
+  props: {
+    currentQuestion: Object
+  }
+}
+</script>
